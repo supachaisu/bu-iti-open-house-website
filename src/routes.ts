3185a5df5c22be4b6d3c7b452f49899da8483d1f
supachaisu <180935381+supachaisu@users.noreply.github.com>
2024-09-11 15:06:37 +0700

    add /how-to-get-here and /social-media in routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -1,16 +1,18 @@
 export const navigation = [
-  { name: "หน้าแรก", href: "/" },
-  { name: "ลงทะเบียน", href: "/registration" },
-  { name: "เวิร์คช็อปและกิจกรรม", href: "/workshops-and-activities" },
-  { name: "กำหนดการ", href: "/schedule" },
-  { name: "วิทยากร", href: "/speakers" },
-  { name: "แกลเลอรี่", href: "/gallery" },
-  { name: "คำถามที่พบบ่อย", href: "/faq" },
-  { name: "ผู้สนับสนุน", href: "/sponsors" },
-  { name: "เกี่ยวกับเรา", href: "/about" },
-  { name: "ติดต่อเรา", href: "/contact" },
-] as const;
+  { name: 'หน้าแรก', href: '/' },
+  { name: 'ลงทะเบียน', href: '/registration' },
+  { name: 'เวิร์คช็อปและกิจกรรม', href: '/workshops-and-activities' },
+  { name: 'กำหนดการ', href: '/schedule' },
+  { name: 'วิทยากร', href: '/speakers' },
+  { name: 'แกลเลอรี่', href: '/gallery' },
+  { name: 'คำถามที่พบบ่อย', href: '/faq' },
+  { name: 'ผู้สนับสนุน', href: '/sponsors' },
+  { name: 'เกี่ยวกับเรา', href: '/about' },
+  { name: 'ติดต่อเรา', href: '/contact' },
+  { name: 'การเดินทาง', href: '/how-to-get-here' },
+  { name: 'Social Media', href: '/social-media' },
+] as const
 
 export const navigationMap = new Map(
-  navigation.map(({ name, href }) => [name, href])
-);
+  navigation.map(({ name, href }) => [name, href]),
+)
